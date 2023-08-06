af3173e88b04750abe35d40435bd171450ba1643
Mauro Peniel <99766054+restenPiz@users.noreply.github.com>
2023-08-06 14:27:03 +0200

    Changing the widget

--- event_app/lib/main.dart
+++ event_app/lib/main.dart
@@ -7,35 +7,8 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      //Inicio de todos os componentes do meu applicativo
-    );
+    return Container();
   }
 }
