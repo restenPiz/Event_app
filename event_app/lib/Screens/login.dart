d762b8134a0f0d5a6ebac18dc0e27d7cdbfffef0
Mauro Peniel <99766054+restenPiz@users.noreply.github.com>
2023-08-07 13:30:42 +0200

    Creating some components to complete the application

---
+++ event_app/lib/Screens/login.dart
@@ -0,0 +1,16 @@
+
+import 'package:flutter/cupertino.dart';
+
+class login extends StatefulWidget {
+  const login({super.key});
+
+  @override
+  State<login> createState() => _loginState();
+}
+
+class _loginState extends State<login> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
