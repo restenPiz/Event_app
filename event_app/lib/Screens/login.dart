d9ec46655a2834386a2ce6cdb0cb9115a0b369d0
Mauro Peniel <99766054+restenPiz@users.noreply.github.com>
2023-08-07 13:47:25 +0200

    Some updates in my application

--- event_app/lib/Screens/login.dart
+++ event_app/lib/Screens/login.dart
@@ -1,5 +1,6 @@
 
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class login extends StatefulWidget {
   const login({super.key});
@@ -11,6 +12,21 @@ class login extends StatefulWidget {
 class _loginState extends State<login> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        TextField(
+          decoration: InputDecoration(
+            labelText: 'Nome de Usuario'
+          ),
+        ),
+        TextField(
+          decoration: InputDecoration(
+            labelText: 'Senha',
+          ),
+        )
+
+      ],
+    );
   }
 }
