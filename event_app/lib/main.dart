7f29b0d849839a5ec86a17b1fcf375f9e2d6ed78
Mauro Peniel <99766054+restenPiz@users.noreply.github.com>
2023-08-07 13:28:11 +0200

    Removing some itens in the application

--- event_app/lib/main.dart
+++ event_app/lib/main.dart
@@ -32,11 +32,9 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
 
   void _incrementCounter() {
     setState(() {
-      _counter++;
     });
   }
 
@@ -49,18 +47,7 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
+        child: Column(),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
