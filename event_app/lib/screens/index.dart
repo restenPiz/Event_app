00e5032e3b59eb37ac18fe40aaf31be6c21ff61e
Mauro Peniel <99766054+restenPiz@users.noreply.github.com>
2023-08-08 20:02:57 +0200

    Resolving the bug off put the cards in the column git add .

--- event_app/lib/screens/index.dart
+++ event_app/lib/screens/index.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 import 'package:flutter/material.dart';
 
 // ignore: camel_case_types
@@ -15,18 +15,35 @@ class index extends StatelessWidget {
           backgroundColor: Colors.amberAccent,
           ),
         body: Center(
-          child: Card(
-            elevation: 4,
-            child: Column(
-              mainAxisSize: MainAxisSize.max,
-              // ignore: prefer_const_literals_to_create_immutables
-              children: [
-                ListTile(
-                  title: Text('Mauro Peniel'),
-                  subtitle: Text('Eu sou mauro peniel'),
+          child: Column(
+            children: [
+              Card(
+                elevation: 4,
+                child: Column(
+                  mainAxisSize: MainAxisSize.max,
+                  // ignore: prefer_const_literals_to_create_immutables
+                  children: [
+                    ListTile(
+                      title: Text('Mauro Peniel'),
+                      subtitle: Text('Eu sou mauro peniel'),
+                    ),
+                  ],
                 ),
-              ],
-            ),
+              ),
+              Card(
+                elevation: 4,
+                child: Column(
+                  mainAxisSize: MainAxisSize.max,
+                  // ignore: prefer_const_literals_to_create_immutables
+                  children: [
+                    ListTile(
+                      title: Text('Mauro Peniel'),
+                      subtitle: Text('Eu sou mauro peniel'),
+                    ),
+                  ],
+                ),
+              ),
+            ],
           ),
         ),
       ),
